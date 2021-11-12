01045b86a10ab5da972dcc71fd0d522d19b66b65
Sam Fan <samisfan@msn.com>
2021-11-11 21:20:21 -0700

    added dimple to vial

--- keyboards/lazydesigners/dimple/keymaps/vial/config.h
+++ keyboards/lazydesigners/dimple/keymaps/vial/config.h
@@ -3,3 +3,6 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xB0, 0x1F, 0x29, 0xF7, 0xC1, 0x01, 0xF6, 0xB1}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
